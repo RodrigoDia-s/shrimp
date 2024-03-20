a642976d561a4f880133a8385be28b911a34f4ab
Rodrigo <diasrod23@gmail.com>
2024-03-19 21:13:26 -0300

    mudancas

--- src/js/default.js
+++ src/js/default.js
@@ -3,17 +3,17 @@ document.getElementById('btnSim').addEventListener('click', function() {
   });
   
   document.getElementById('btnNao').addEventListener('click', function() {
-    window.location.href = 'terceira_pagina.html';
+    window.location.href = './src/terceira_pagina.html';
   });
   
   document.getElementById('btnNaoPopup').addEventListener('click', function() {
-    window.location.href = 'terceira_pagina.html';
+    window.location.href = './src/terceira_pagina.html';
   });
   
   document.getElementById('btnSimPopup').addEventListener('click', function() {
     // Aqui você pode adicionar a lógica para o botão "Sim" do popup, como redirecionar para outra página
     // Por exemplo:
-    window.location.href = 'camaraokk.html';
+    window.location.href = './src/camaraokk.html';
   });
   
   document.getElementById('btnNaoPopup').addEventListener('click', function() {
